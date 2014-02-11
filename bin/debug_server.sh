fe0c119568992c908d8164359082df5f4c9f0b15
Edmond Meinfelder <edmond@stdarg.com>
2014-02-10 16:43:03 -0800

    Bash scripts to start the client and server.

---
+++ bin/debug_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export NODE_ENV="DEVELOPMENT"
+
+if [ ! -f "./bin/server.js" ]
+then
+    echo "Run this script is the root directory of blackjack."
+    echo "That is where the package.json resides."
+    exit 1
+fi
+
+
+./bin/server.js
+
